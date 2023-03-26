c4ec9493703174862c8fcb4e165cead95880353a
GHDBASHEN <ghdbashen@gmail.com>
2023-03-26 14:47:08 +0530

    🎨 IMPROVE:

--- functions/Stock/stock.c
+++ functions/Stock/stock.c
@@ -9,19 +9,25 @@ typedef struct {
 } Product;
 
 void search_stock_product(){
-  
+     int product_stock_id;
+    printf("Insert the Product ID: ");
+    scanf("%d",&product_stock_id);
+    FILE *productTable;
+    productTable = fopen("../../data/products.txt", "r");
+    Product product;
+    char search_st_product[400];
+    printf("%s\t %s\t\t %s\n", "Id", "Name", "Description");
+    printf("--\t --\t\t --\n");
+
+    while (fgets(search_st_product, sizeof(search_st_product), productTable) != NULL) {
+      sscanf(search_st_product, "%d, %99[^,], %299[^,]", &product.id, product.name,
+            product.description);
+        if(product_stock_id == product.id){
+      printf("%d\t %s\t %s", product.id, product.name, product.description);}
+    }
+    fclose(productTable);
 }
  void view_all_stock_product(){
-    // Product product;
-    // printf("\tid,\tname,\tquantity");
-    // FILE *prod;
-    // prod=fopen("./data/products.txt","r");
-    // char prd_array[100];
-    // while(fgets(prd_array,100,prod)){
-    //     sscanf(prd_array, "%d, %99[^,], %d", &product.id, product.name,
-    //        product.quantity);
-    //     printf("%d %s %d \n", product.id, product.name, product.quantity);
-    // }
     FILE *productTable;
     productTable = fopen("../../data/products.txt", "r");
     Product product;
@@ -30,11 +36,10 @@ void search_stock_product(){
     printf("--\t --\t\t --\n");
 
     while (fgets(view_stock_product, sizeof(view_stock_product), productTable) != NULL) {
-      sscanf(view_stock_product, "%d, %99[^,], %299[^,]", &product.id, product.name,
-            product.description);
+      sscanf(view_stock_product, "%d, %99[^,], %299[^,]", &product.id, product.name, product.description);
       printf("%d\t %s\t %s", product.id, product.name, product.description);
     }
-
+    fclose(productTable);
  }
  void view_single_stock_product(){
     int product_stock_id;
@@ -53,6 +58,7 @@ void search_stock_product(){
         if(product_stock_id == product.id){
       printf("%d\t %s\t %s", product.id, product.name, product.description);}
     }
+    fclose(productTable);
  }
  int main(){
     int operation;
