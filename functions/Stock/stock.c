f49fe6bd7b38c7476a7050d5272db81bafe2669f
GHDBASHEN <ghdbashen@gmail.com>
2023-03-26 11:30:30 +0530

    core dummped

--- functions/Stock/stock.c
+++ functions/Stock/stock.c
@@ -1,22 +1,17 @@
 #include<stdio.h>
 #include<string.h>
-// #define PRODUCT_TABLE "./data/products.txt"
-//  void search_product();
-//  void view_all_product();
-//  void view_single_product();
-
 typedef struct {
   int id;
   char name[100];
   char description[300];
-//   float price;
+// float price;
   int quantity;
 } Product;
 
-void search_product(){
+void search_stock_product(){
   
 }
- void view_all_product(){
+ void view_all_stock_product(){
     // Product product;
     // printf("\tid,\tname,\tquantity");
     // FILE *prod;
@@ -27,22 +22,37 @@ void search_product(){
     //        product.quantity);
     //     printf("%d %s %d \n", product.id, product.name, product.quantity);
     // }
-      FILE *productTable;
-  productTable = fopen("products.txt", "r");
-  Product product;
-  char line[400];
-  printf("%s\t %s\t\t %s\n", "Id", "Name", "Description");
-  printf("--\t --\t\t --\n");
+    FILE *productTable;
+    productTable = fopen("./data/products.txt", "r");
+    Product product;
+    char view_stock_product[400];
+    printf("%s\t %s\t\t %s\n", "Id", "Name", "Description");
+    printf("--\t --\t\t --\n");
 
-  while (fgets(line, sizeof(line), productTable) != NULL) {
-    sscanf(line, "%d, %99[^,], %299[^,]", &product.id, product.name,
-           product.description);
-    printf("%d\t %s\t %s", product.id, product.name, product.description);
-  }
+    while (fgets(view_stock_product, sizeof(view_stock_product), productTable) != NULL) {
+      sscanf(view_stock_product, "%d, %99[^,], %299[^,]", &product.id, product.name,
+            product.description);
+      printf("%d\t %s\t %s", product.id, product.name, product.description);
+    }
 
  }
- void view_single_product(){
+ void view_single_stock_product(){
+    int product_stock_id;
+    printf("Insert the Product ID: ");
+    scanf("%d",&product_stock_id);
+    FILE *productTable;
+    productTable = fopen("./data/products.txt", "r");
+    Product product;
+    char single_stock_product[400];
+    printf("%s\t %s\t\t %s\n", "Id", "Name", "Description");
+    printf("--\t --\t\t --\n");
 
+    while (fgets(single_stock_product, sizeof(single_stock_product), productTable) != NULL) {
+      sscanf(single_stock_product, "%d, %99[^,], %299[^,]", &product.id, product.name,
+            product.description);
+        if(product_stock_id == product.id){
+      printf("%d\t %s\t %s", product.id, product.name, product.description);}
+    }
  }
  int main(){
     int operation;
@@ -54,13 +64,13 @@ void search_product(){
     
     switch (operation){
         case 1:
-            search_product();
+            search_stock_product();
             break;
         case 2:
-            view_all_product();
+            view_all_stock_product();
             break;
         case 3:
-            view_single_product();
+            view_single_stock_product();
             break;
     }
 }
